14c3d00c17ace4ed7b6afcb0f8797ddaf5ffb22b
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2024-10-22 01:35:04 +0100

    chore: ajustes

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -4,17 +4,15 @@ const UserController = require('../../../controllers/Users');
 
 const userController = new UserController();
 
-
-
 router.post('/login', userController.login);//testado
 router.post('/register', userController.store);//testado
 router.put('/', auth.required, userController.update);//testado
 router.delete('/', auth.required, userController.remove);//testado
 
-router.get('/recovery-password', userController.showRecovery);
-router.post('/recovery-password', userController.createRecovery);
-router.get('/password-recovery', userController.showCompleteRecovery);
-router.post('/password-recovery', userController.completeRecovery);
+router.get('/recovery-password', userController.showRecovery);//testado
+router.post('/recovery-password', userController.createRecovery);//testado
+router.get('/password-recovery', userController.showCompleteRecovery);//testado
+router.post('/password-recovery', userController.completeRecovery);//testado
 
 router.get('/', auth.required, userController.index);//testado
 router.get('/:id', auth.required, userController.show);//testado
